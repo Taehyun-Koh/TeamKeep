121b7d28870c1bf39b64bdccde5cc88514c6af4a
mlkwy <sorento20081@gmail.com>
2022-11-11 17:00:14 +0900

    Changes to the createCard() and addEntry()

--- scripts.js
+++ scripts.js
@@ -14,27 +14,30 @@ const FileType = {
 }
 
 function addEntry(entry) {
+    console.log(entries);
     entries.push(entry);
 
     let card = createCard(entry);
     let tilecontainer = document.querySelector("#alltiles");
     tilecontainer.appendChild(card);
 
-    if(entry.filetype == FileType.Image) {
-        tilecontainer = document.querySelector("#imagetiles");
-        let dupcard = card.cloneNode(true);
-        tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
+    if(entry.tiletype != TileType.Temp) {
+        if(entry.filetype == FileType.Image) {
+            tilecontainer = document.querySelector("#imagetiles");
+            let dupcard = card.cloneNode(true);
+            tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
+        }
+        if(entry.filetype == FileType.File) {
+            tilecontainer = document.querySelector("#filetiles");
+            let dupcard = card.cloneNode(true);
+            tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
+        }
+        if(entry.filetype == FileType.URL) {
+            tilecontainer = document.querySelector("#urltiles");
+            let dupcard = card.cloneNode(true);
+            tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
+        }
     }
-    if(entry.filetype == FileType.File) {
-        tilecontainer = document.querySelector("#filetiles");
-        let dupcard = card.cloneNode(true);
-        tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
-    }
-    if(entry.filetype == FileType.URL) {
-        tilecontainer = document.querySelector("#urltiles");
-        let dupcard = card.cloneNode(true);
-        tilecontainer.appendChild(dupcard); // to do : need to append tile to the html
-    }		
 }
 
 function createCard(entry) {
@@ -58,38 +61,46 @@ function createCard(entry) {
     title.appendChild(titletext);
     cardbody.appendChild(title);
 
-    let inputarea = document.createElement("div");
-    inputarea.className = "input-group mb-3";
-    cardbody.appendChild(inputarea);
+    if(entry.tiletype == TileType.Temp) {
+        let inputarea = document.createElement("div");
+        inputarea.className = "input-group mb-3";
+        cardbody.appendChild(inputarea);
 
-    let inputtext = document.createElement("input");
-    inputtext.className = "form-control";
-    inputtext.type = "text";
-    inputtext.placeholder = "Provide a description of the file";
-    inputarea.appendChild(inputtext);
+        let inputtext = document.createElement("input");
+        inputtext.className = "form-control";
+        inputtext.type = "text";
+        inputtext.style = "font-size: smaller";
+        inputtext.placeholder = "Provide a description of the file";
+        inputarea.appendChild(inputtext);
 
-    let appendarea = document.createElement("div");
-    appendarea.className = "input-group-append";
-    inputarea.appendChild(appendarea);
+        let appendarea = document.createElement("div");
+        appendarea.className = "input-group-append";
+        inputarea.appendChild(appendarea);
 
-    let uploadbutton = document.createElement("button");
-    uploadbutton.className = "btn btn-outline-secondary";
-    uploadbutton.type = "button";
-    let uploadicon = document.createElement("i");
-    uploadicon.className="bi bi-file-earmark-arrow-up";
-    uploadbutton.appendChild(uploadicon);
-    appendarea.appendChild(uploadbutton);
+        let uploadbutton = document.createElement("button");
+        uploadbutton.className = "btn btn-outline-secondary";
+        uploadbutton.type = "button";
+        let uploadicon = document.createElement("i");
+        uploadicon.className="bi bi-file-earmark-arrow-up";
+        uploadbutton.appendChild(uploadicon);
+        appendarea.appendChild(uploadbutton);
     
-    uploadicon.addEventListener("click", () => {
-        entry.desc = inputtext.value;
-        inputarea.remove();
-        let desc = document.createElement("h7");
-        desc.className = "card-text";
-        desc.style = "color: grey;"
-        let desctext = document.createTextNode(entry.desc);
-        desc.appendChild(desctext);
-        cardbody.appendChild(desc);
-    });
+        uploadicon.addEventListener("click", () => {
+            entry.desc = inputtext.value;
+            entry.tiletype = entry.filetype;
+            card.remove();
+            addEntry(entry);
+        });
+
+        return card;
+    }
+
+    let desc = document.createElement("h7");
+    desc.className = "card-text";
+    desc.style = "color: grey; font-size: smaller;"
+    let desctext = document.createTextNode(entry.desc);
+    desc.appendChild(desctext);
+    cardbody.appendChild(desc);
 
     return card;
 }
@@ -102,11 +113,11 @@ let addentrybutton = document.querySelector("#addentrybutton");
 let addthispagebutton = document.querySelector("#addthispagebutton");
 
 addentrybutton.addEventListener("click", () => {
-    // document.querySelector("#fileinput").click();
-    console.log("hi");
-    $('#addnotesmodal').modal('show'); //show modal
-    $('#btn-n-save').hide();
-    $('#btn-n-add').show();
+    document.querySelector("#fileinput").click();
+    document.querySelector("#pills-all-tab").click();
+    //$('#addnotesmodal').modal('show'); //show modal
+    //$('#btn-n-save').hide();
+    //$('#btn-n-add').show();
 });
 
 let fileinput = document.querySelector("#fileinput");
@@ -133,7 +144,7 @@ fileinput.addEventListener("change", (event) => {
     filereader.readAsDataURL(file);
 
     filereader.onload = function() {
-         let entry = {
+        let entry = {
             // defines tile viewing option
             tiletype: TileType.Temp,
         
@@ -148,6 +159,7 @@ fileinput.addEventListener("change", (event) => {
             desc: '',
         }
         addEntry(entry);
+        fileinput.value=''; 
     }
     // to do : view input tile, add event listner to them to upload, cancel, ... 
 });

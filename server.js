4c3e0ecdb322f119b7ed34371e5447da8db0f1d5
kothe <taehyun1018@gmail.com>
2022-11-18 11:35:55 +0900

    Nodejs와 html/css/js 연동

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-var express = require('express')
-var app = express()
-var http = require('http')
+const http = require("http");
+const express = require("express");
 var router = express.Router()
 var mysql = require('mysql')
 var fs = require('fs')
 var ejs = require('ejs')
+const app = express();
+const server = http.createServer(app);
+const PORT = 3000;
 
-app.use(express.static("public"))
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-    })
-    
-    app.listen(3000);
+const WEBPATH = "./webpage";
+
+app.use(express.static(__dirname + "/public"));
+
+// app.get("/", (req, res) => {
+//     fs.readFile(`${WEBPATH}/index.html`, (error, data) => {
+//         if (error) {
+//             console.log(error);
+//             return res.status(500).send("<h1>500 Error!</h1>");
+//         }
+//         res.writeHead(200, { "Content-Type": "text/html" });
+//         res.end(data);
+//     });
+// });
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});

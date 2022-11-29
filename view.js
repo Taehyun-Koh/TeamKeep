a7c1c905e93df1f6ae3e4213c8eab120d8cdeea1
kothe <taehyun1018@gmail.com>
2022-11-29 19:41:02 +0900

    Add password in card

--- view.js
+++ view.js
@@ -8,6 +8,13 @@ const connection = mysql.createConnection({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME_ROOM
 });
+
+const connection_info = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME_ROOMINFO
+});
 /* -------------------------- ESTABLISH CONNECTION -------------------------- */
 
 
@@ -79,23 +86,53 @@ function createTeamCard(teamname, i, belong) {
     card_text.style.fontSize = "smaller";
     card_text.style.opacity = "0.5";
     card_text.innerHTML = file_num[i] + " items";
+
+    var pw_input = document.createElement("input");
+    pw_input.setAttribute("id", teamname+'pw');
+
     var btn = document.createElement("button");
     if (belong === true) {
         btn.className = "btn btn-primary";
         btn.innerHTML = "Enter";
+        card_body.appendChild(card_title);
+        card_body.appendChild(card_text);
+        card_body.appendChild(btn);
+
+        btn.onclick = function (e) {
+            localStorage.setItem("teamname", teamname);
+            document.location.href = 'index.html';
+        }
     }
     else if (belong === false) {
         btn.className = "btn btn-dark";
         btn.innerHTML = "Join";
+        card_body.appendChild(card_title);
+        card_body.appendChild(card_text);
+        card_body.appendChild(pw_input);
+        card_body.appendChild(btn);
+        btn.onclick = function (e) {
+            localStorage.setItem("teamname", teamname);
+            var tn = localStorage.getItem("teamname");
+            let team_pw;
+            connection_info.query("SELECT distinct pw FROM " + tn, function (error, results, fields) {
+                if (error) throw error;
+                team_pw = results[0].pw;
+                var password = document.getElementById(teamname+'pw').value;
+                console.log(password);
+                console.log(team_pw);
+                if (password){
+                    if (password === team_pw) document.location.href = 'index.html';
+                    else alert("비밀번호가 틀렸습니다.");
+                }
+                else{
+                    alert("비밀번호를 입력하세요.");
+                }
+            });
+        }
     }
 
-    btn.onclick = function (e) {
-        localStorage.setItem("teamname", teamname);
-        document.location.href = 'index.html';
-    }
-    card_body.appendChild(card_title);
-    card_body.appendChild(card_text);
-    card_body.appendChild(btn);
+
+
     return card;
 }
 /* ------------------------------ TEAM 카드 생성 ------------------------------ */
@@ -203,3 +240,53 @@ logoutbutton.addEventListener("click", () => {
     document.location.href="login.html";
 })
 /* ------------------------------ LOGOUT BUTTON ----------------------------- */
+
+
+
+function modal(id) {
+    var zIndex = 9999;
+    var modal = document.getElementById(id);
+
+    // 모달 div 뒤에 희끄무레한 레이어
+    var bg = document.createElement('div');
+    bg.setStyle({
+        position: 'fixed',
+        zIndex: zIndex,
+        left: '0px',
+        top: '0px',
+        width: '100%',
+        height: '100%',
+        overflow: 'auto',
+        // 레이어 색갈은 여기서 바꾸면 됨
+        backgroundColor: 'rgba(0,0,0,0.4)'
+    });
+    document.body.append(bg);
+
+    // 닫기 버튼 처리, 시꺼먼 레이어와 모달 div 지우기
+    modal.querySelector('.modal_close_btn').addEventListener('click', function() {
+        bg.remove();
+        modal.style.display = 'none';
+    });
+
+    modal.setStyle({
+        position: 'fixed',
+        display: 'block',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+
+        // 시꺼먼 레이어 보다 한칸 위에 보이기
+        zIndex: zIndex + 1,
+
+        // div center 정렬
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        msTransform: 'translate(-50%, -50%)',
+        webkitTransform: 'translate(-50%, -50%)'
+    });
+}
+
+// Element 에 style 한번에 오브젝트로 설정하는 함수 추가
+Element.prototype.setStyle = function(styles) {
+    for (var k in styles) this.style[k] = styles[k];
+    return this;
+};
